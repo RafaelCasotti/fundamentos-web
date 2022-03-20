788ae43fa3e475fbb7b0722ed392cf6828b8f50f
Rafael Casotti <rafa_scasotti@hotmail.com>
2022-03-20 20:01:48 -0300

    @RafaelCasotti Task: Atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let mapa = document.querySelector('#mapa')
+let mensagem = document.querySelector('#mensagem')
+
 let nomeOk = false
 let emailOk = false
 let assuntoOk = false
-let mapa = document.querySelector('#mapa')
+let mensagemOk = false
+
 //aumentar o tamanho do campo nome e email
 nome.style.width="100%"
 email.style.width="100%"
@@ -13,10 +17,10 @@ function validaNome(){
 
     let txtNome = document.querySelector('#txtNome')
     if (nome.value.length < 3){
-        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.innerHTML = 'Nome Inválido ❌'
         txtNome.style.color = 'red'
     } else{
-        txtNome.innerHTML = 'Nome Válido'
+        txtNome.innerHTML = 'Nome Válido ✔️'
         txtNome.style.color = 'green'
         nomeOk = true      
     }
@@ -25,10 +29,10 @@ function validaNome(){
     function validaEmail(){
     let txtEmail = document.querySelector('#txtEmail')
     if(email.value.indexOf('@')== -1 || email.value.indexOf('.') == -1){
-        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.innerHTML = 'E-mail inválido ❌'
         txtEmail.style.color = 'red'
     } else{
-        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.innerHTML = 'E-mail válido ✔️'
         txtEmail.style.color = 'green'
         emailOk = true
     }
@@ -37,7 +41,7 @@ function validaNome(){
     function validaAssunto(){
     let txtAssunto = document.querySelector('#txtAssunto')
     if(assunto.value.length >= 100){
-        txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres'
+        txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres ❗'
         txtAssunto.style.color= 'red'
         txtAssunto.style.display='block'
         assuntoOk = false
@@ -49,9 +53,9 @@ function validaNome(){
 
         function enviar(){
             if(nomeOk == true && emailOk && assuntoOk == true){
-                alert('Formulário enviado com sucesso!')
+                alert('Formulário enviado com sucesso! ✔️')
             } else {
-                alert('Preencha o formulário corretamente antes de enviar...')
+                alert('Preencha o formulário corretamente antes de enviar... ❗')
             }
         }
 
@@ -65,3 +69,24 @@ function validaNome(){
             mapa.style.width = '400px'
             mapa.style.height = '250px'
         }    
+    
+        function enviar2(){
+            if(mensagemOk == true){
+                alert('Formulário enviado com sucesso! ✔️')
+            } else {
+                alert('Preencha o formulário corretamente antes de enviar... ❗')
+            }
+        }
+
+        function validaMensagem(){
+            let txtAssunto = document.querySelector('#txtMensagem')
+            if(mensagem.value.length >= 100){
+                txtMensagem.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres ❗'
+                txtMensagem.style.color= 'red'
+                txtMensagem.style.display='block'
+                mensagemOk = false
+            } else {
+                txtMensagem.style.display='none'
+                mensagemOk = true
+            }
+        }
